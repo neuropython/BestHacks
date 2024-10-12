5f7b2e51403d3f7d43e7bbd689a041a9a26427ca
Damian <damianpietron007@gmail.com>
2024-10-12 14:49:35 +0200

    Create insert_one contoller

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from models.user_model import User, UserInDB, Tags, UserType
+
 
 load_dotenv()
 
@@ -20,3 +22,39 @@ class DB:
     def __init__(self):
         self.client = MongoClient(MONGO_URI)
         self.db = self.client['BestHacks']
+
+    def create_new_user(self, user: UserInDB):
+        result = self.db['Users'].insert_one(user.dict())
+        if result.inserted_id:
+            return {"message": "User created successfully"}
+        else:
+            return {"message": "Error creating user"}
+
+    def get_user(self, username: str):
+        user = self.db['Users'].find_one({"username": username})
+        if user:
+            return User(**user)
+        else:
+            return {"message": "User not found"}
+        
+if __name__ == "__main__":
+    db = DB()
+    new_user = UserInDB(
+        name="John",
+        second_name="Doe",
+        bio="This is a bio",
+        email="user@example.com",
+        username="user1",
+        password="password123",
+        profile_picture="profile.jpg",
+        how_many_requests=0,
+        user_type=UserType.Student,
+        tags=[Tags.Languages],
+        academical_index="A"
+    )
+    db_instance = DB()
+    msg = db_instance.create_new_user(new_user)
+    print(msg)
+
+
+        
