2250ecc8f78862b39c365fffad5091816e1156b7
Damian <damianpietron007@gmail.com>
2024-10-13 05:15:41 +0200

    Refactor NoteInDB model to correctly retrieve owner information

--- models/note_model.py
+++ models/note_model.py
@@ -13,12 +13,16 @@ class Note(BaseModel):
     owner: Optional[str] = None
     owner_picture: Optional[str] = None
     send_to: Optional[str] = None
-    accepted: bool
+    accepted: Optional[bool] = None
 
 class NoteInDB(Note):
+    accepted: bool = False
+
     def __init__(self, **data):
         super().__init__(**data)
-        user = db['Users'].find_one({"id": self.owner_id})
+        if self.owner_id[0] == '"':
+            self.owner_id = self.owner_id[1:-1]
+        user = db['Users'].find_one({"id": str(self.owner_id)})
         self.owner = user['username']
         self.owner_picture = user['profile_picture']
         sender = db['Users'].find_one({"id": self.send_to_id})
